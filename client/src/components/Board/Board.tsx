8204a6fe3a43f3ebef8c303390453e7661bf6b7d
Oleksandr Riazantsev <riazantsev.aleks@gmail.com>
2024-04-26 13:55:52 -0400

    client side initial commit

---
+++ client/src/components/Board/Board.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { MoreHorizontal } from "react-feather";
+
+import Card from "../Card/Card";
+import Dropdown from "../Dropdown/Dropdown";
+import CustomInput from "../CustomInput/CustomInput";
+
+import "./Board.css";
+import { IBoard, ICard } from "../../interfaces/kanban";
+
+interface BoardProps {
+  board: IBoard;
+  addCard: (boardId: number, title: string) => void;
+  removeBoard: (boardId: number) => void;
+  removeCard: (boardId: number, cardId: number) => void;
+  onDragEnd: (boardId: number, cardId: number) => void;
+  onDragEnter: (boardId: number, cardId: number) => void;
+  updateCard: (boardId: number, cardId: number, card: ICard) => void;
+}
+
+function Board(props: BoardProps) {
+  const {
+    board,
+    addCard,
+    removeBoard,
+    removeCard,
+    onDragEnd,
+    onDragEnter,
+    updateCard,
+  } = props;
+  const [showDropdown, setShowDropdown] = useState(false);
+  console.log(showDropdown)
+
+  function onClose(id: number) {
+    setShowDropdown(false);
+    removeBoard(id);
+  }
+
+  return (
+    <div className="board">
+      <div className="board-inner" key={board?.id}>
+        <div className="board-header">
+          <p className="board-header-title">
+            {board?.title}
+            <span>{board?.cards?.length || 0}</span>
+          </p>
+          <div
+            className="board-header-title-more"
+          >
+            <MoreHorizontal onClick={() => setShowDropdown(prev => !prev)}/>
+            {showDropdown && (
+              <Dropdown
+                class="board-dropdown"
+              >
+                <p onClick={() => onClose(board?.id)}>Delete Board</p>
+              </Dropdown>
+            )}
+          </div>
+        </div>
+        <div className="board-cards">
+          {board?.cards?.map((item) => (
+            <Card
+              key={item.id}
+              card={item}
+              boardId={board.id}
+              removeCard={removeCard}
+              onDragEnter={onDragEnter}
+              onDragEnd={onDragEnd}
+              updateCard={updateCard}
+            />
+          ))}
+          <CustomInput
+            text="+ Add Card"
+            placeholder="Enter Card Title"
+            displayClass="board-add-card"
+            editClass="board-add-card-edit"
+            onSubmit={(value: string) => addCard(board?.id, value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;
