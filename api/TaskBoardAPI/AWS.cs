b1b913b71031e4ed5d57aba19effffdb02aa6bf1
Oleksandr Riazantsev <riazantsev.aleks@gmail.com>
2024-05-27 13:06:53 -0400

    api: remove unnecessary file, adjust aws class properties

--- api/TaskBoardAPI/AWS.cs
+++ api/TaskBoardAPI/AWS.cs
@@ -4,4 +4,6 @@ public class AWS
 {
     public string? Region { get; set; }
     public string? UserPoolId { get; set; }
+    public string? ClientId { get; set; }
+    public string? ClientSecret { get; set; }
 }
